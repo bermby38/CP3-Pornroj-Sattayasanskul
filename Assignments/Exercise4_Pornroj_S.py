6f59670979b521cdb98af70edea08e189534e635
bermby38 <30828158+bermby38@users.noreply.github.com>
2020-09-17 20:15:41 +0700

    Create Exercise4_Pornroj_S.py

---
+++ Assignments/Exercise4_Pornroj_S.py
@@ -0,0 +1,15 @@
+FEscore = 95.0
+GBscore = 78.5
+ICSscore = 84.0
+CPscore = 90.4
+
+print("--- Your Score---")
+print()
+print("Foundation English :", FEscore)
+print()
+print("General Business :", GBscore)
+print()
+print("Introduction to Computer System :", ICSscore)
+print()
+print("Computer Programming :", CPscore)
+print()
