eff717cfc4d5989b5a4dc2c62d16eaaecb5e3ef7
bermby38 <bermby38@hotmail.com>
2020-10-06 11:18:24 +0700

    commit

--- Assignments/Exercise11_Pornroj_S.py
+++ Assignments/Exercise11_Pornroj_S.py
@@ -2,5 +2,5 @@ inputNumber = int((input("Input Number : ")))
 textStar = ""
 
 for i in range(inputNumber):
-    textStar = (" "*(inputNumber-i)+("*"*(2*i+1)))
+    textStar = (" "*(inputNumber-i-1)+("*"*(2*i+1)))
     print(textStar)
