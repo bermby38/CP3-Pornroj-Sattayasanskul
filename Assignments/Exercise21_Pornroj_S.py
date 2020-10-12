de915eb140ef1f77f8d31f357f5f522520262016
bermby38 <bermby38@hotmail.com>
2020-10-12 14:09:43 +0700

    Update Exercise21_Pornroj_S.py

--- Assignments/Exercise21_Pornroj_S.py
+++ Assignments/Exercise21_Pornroj_S.py
@@ -2,9 +2,32 @@ from tkinter import *
 import math
 
 
-def leftclickbutton(event):
-    labelResult.configure(text=float(textboxWeight.get()) / math.pow(float(textboxHeight.get()) / 100, 2))
+def calculate_bmi(height, weight):
+    height = float(height)
+    weight = float(weight)
+    bmi = weight / math.pow(height/100, 2)
 
+    bmi_result = ""
+    if bmi >= 30.0:
+        bmi_result = "อ้วนมาก"
+    elif bmi >= 25.0:
+        bmi_result = "อ้วน"
+    elif bmi >= 23.0:
+        bmi_result = "น้ำหนักเกิน"
+    elif bmi >= 18.6:
+        bmi_result = "น้ำหนักปกติ"
+    elif bmi < 18.6:
+        bmi_result = "ผอมเกินไป"
+
+    return bmi_result
+
+
+def leftclickbutton(event):
+    bmi_result = calculate_bmi(textboxHeight.get(), textboxWeight.get())
+    labelResult.configure(text=bmi_result)
+
+
+#####GUI Setup#####
 
 mainWindow = Tk()
 
@@ -19,8 +42,7 @@ textboxWeight.grid(row=1, column=1)
 buttonCalculate = Button(mainWindow, text="คำนวณ BMI")
 buttonCalculate.grid(row=2, column=0)
 buttonCalculate.bind('<Button-1>', leftclickbutton)
-labelResult = Label(mainWindow,text="ผลลัพธ์")
+labelResult = Label(mainWindow, text="ผลลัพธ์")
 labelResult.grid(row=2, column=1)
-#labelResult.configure(text="TEst")
 
 mainWindow.mainloop()
